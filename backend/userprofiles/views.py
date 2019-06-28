305bccdba9b4be6a7d58c3f0d61f5025860b64d1
Spectre <bowo@anakdesa.id>
2019-06-28 21:39:34 +0700

    Add views

--- backend/userprofiles/views.py
+++ backend/userprofiles/views.py
@@ -1,3 +1,29 @@
-from django.shortcuts import render
+from rest_framework import generics, mixins, viewsets
+from rest_framework.filters import SearchFilter
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.viewsets import ModelViewSet
 
-# Create your views here.
+from .permissions import IsOwnerOrReadOnly, IsOwnProfileOrReadOnly
+from .serializers import (ProfileAvatarSerializer,
+                                      ProfileSerializer)
+from .models import Profile
+
+
+class AvatarUpdateView(generics.UpdateAPIView):
+    serializer_class = ProfileAvatarSerializer
+    permission_classes = [IsAuthenticated]
+
+    def get_object(self):
+        profile_object = self.request.user.profile
+        return profile_object
+
+
+class ProfileViewSet(mixins.UpdateModelMixin,
+                     mixins.ListModelMixin,
+                     mixins.RetrieveModelMixin,
+                     viewsets.GenericViewSet):
+    queryset = Profile.objects.all()
+    serializer_class = ProfileSerializer
+    permission_classes = [IsAuthenticated, IsOwnProfileOrReadOnly]
+    filter_backends = [SearchFilter]
+    search_fields = ["city"]
