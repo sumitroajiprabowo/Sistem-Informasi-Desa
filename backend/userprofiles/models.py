642dd4367d964bda5ceb2827ecd7dad3ddd87341
Spectre <bowo@anakdesa.id>
2019-06-28 15:27:18 +0700

    Add profile models

---
+++ backend/userprofiles/models.py
@@ -0,0 +1,12 @@
+from django.db import models
+from core.models import User
+
+
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    bio = models.CharField(max_length=240, blank=True)
+    city = models.CharField(max_length=30, blank=True)
+    avatar = models.ImageField(null=True, blank=True)
+
+    def __str__(self):
+        return self.user.email
