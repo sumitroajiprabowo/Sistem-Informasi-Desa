d0cf1e319afb9c9a6b2e3e1e8209003d102fe1bb
Spectre <bowo@anakdesa.id>
2019-06-28 21:38:51 +0700

    Update appconfig

--- backend/userprofiles/apps.py
+++ backend/userprofiles/apps.py
@@ -3,10 +3,3 @@ from django.apps import AppConfig
 
 class UserprofilesConfig(AppConfig):
     name = 'userprofiles'
-
-
-class ProfilesConfig(AppConfig):
-    name = 'userprofiles'
-
-    def ready(self):
-        import userprofiles.signals
