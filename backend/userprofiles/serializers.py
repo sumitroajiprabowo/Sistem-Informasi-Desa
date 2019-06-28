5befba95a5cf54ae6e14201b7e6c785d051abaa5
Spectre <bowo@anakdesa.id>
2019-06-28 21:39:19 +0700

    Add profile serializers

---
+++ backend/userprofiles/serializers.py
@@ -0,0 +1,19 @@
+from rest_framework import serializers
+from .models import Profile
+
+
+class ProfileSerializer(serializers.ModelSerializer):
+
+    user = serializers.StringRelatedField(read_only=True)
+    avatar = serializers.ImageField(read_only=True)
+
+    class Meta:
+        model = Profile
+        fields = "__all__"
+
+
+class ProfileAvatarSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = Profile
+        fields = ("avatar")
