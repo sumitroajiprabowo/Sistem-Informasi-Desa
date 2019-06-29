9986466776c909cfda49a7e9ca1e50c14890d09e
Spectre <bowo@anakdesa.id>
2019-06-29 14:34:59 +0700

    Update urlpatters

--- backend/config/urls.py
+++ backend/config/urls.py
@@ -21,8 +21,9 @@ from django.conf import settings
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/areacode/', include('areacode.urls')),
-    path("api/", include("userprofiles.urls")),
+    path("api/user/", include("userprofiles.urls")),
 ]
 
 if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    urlpatterns += static(settings.MEDIA_URL,
+                          document_root=settings.MEDIA_ROOT)
