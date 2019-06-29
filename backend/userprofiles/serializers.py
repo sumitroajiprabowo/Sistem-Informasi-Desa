5a2b324e06f3608a7f3f855ea04eb54b08313225
Spectre <bowo@anakdesa.id>
2019-06-29 14:36:02 +0700

    Add profile status serializers

--- backend/userprofiles/serializers.py
+++ backend/userprofiles/serializers.py
@@ -1,10 +1,14 @@
 from rest_framework import serializers
-from .models import Profile
+from .models import Profile, ProfileStatus
 
 
 class ProfileSerializer(serializers.ModelSerializer):
 
     user = serializers.StringRelatedField(read_only=True)
+    province = serializers.StringRelatedField(read_only=True)
+    regency = serializers.StringRelatedField(read_only=True)
+    district = serializers.StringRelatedField(read_only=True)
+    village = serializers.StringRelatedField(read_only=True)
     avatar = serializers.ImageField(read_only=True)
 
     class Meta:
@@ -16,4 +20,13 @@ class ProfileAvatarSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = Profile
-        fields = ("avatar")
+        fields = ("avatar",)
+
+
+class ProfileStatusSerializer(serializers.ModelSerializer):
+
+    user_profile = serializers.StringRelatedField(read_only=True)
+
+    class Meta:
+        model = ProfileStatus
+        fields = "__all__"
