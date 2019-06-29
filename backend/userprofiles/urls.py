83b5a2d735b76c2053914f0ba0c10f106825b202
Spectre <bowo@anakdesa.id>
2019-06-29 23:25:32 +0700

    Add Urls login and register

--- backend/userprofiles/urls.py
+++ backend/userprofiles/urls.py
@@ -1,6 +1,7 @@
 from django.urls import include, path
 from rest_framework.routers import DefaultRouter
-from .views import (AvatarUpdateView, ProfileViewSet,
+from .views import (CreateUserView, CreateTokenView,
+                    AvatarUpdateView, ProfileViewSet,
                     ProfileStatusViewSet)
 
 router = DefaultRouter()
@@ -10,5 +11,7 @@ router.register(r"status", ProfileStatusViewSet, basename="status")
 
 urlpatterns = [
     path("", include(router.urls)),
-    path("avatar/", AvatarUpdateView.as_view(), name="avatar-update")
+    path("avatar/", AvatarUpdateView.as_view(), name="avatar-update"),
+    path('auth/register/', CreateUserView.as_view(), name='create'),
+    path('token/', CreateTokenView.as_view(), name='token'),
 ]
