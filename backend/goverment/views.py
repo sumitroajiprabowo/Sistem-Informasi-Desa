c662a6b3f8b99318b0ce5a297716afb0ee5c1e90
Spectre <bowo@anakdesa.id>
2019-07-03 19:06:24 +0700

    Add Viewset

--- backend/goverment/views.py
+++ backend/goverment/views.py
@@ -1,3 +1,19 @@
-from django.shortcuts import render
+from rest_framework import viewsets
+from rest_framework.permissions import (IsAuthenticated,
+                                        )
+from .serializers import KelembagaanSerializers, JabatanSerializers
+from .models import Kelembagaan, Jabatan
 
-# Create your views here.
+
+class KelembagaanView(viewsets.ModelViewSet):
+    """Create kelembagaan in the system"""
+    queryset = Kelembagaan.objects.all()
+    serializer_class = KelembagaanSerializers
+    permission_classes = (IsAuthenticated,)
+
+
+class JabatanView(viewsets.ModelViewSet):
+    """Create Jabatan in the system"""
+    queryset = Jabatan.objects.all()
+    serializer_class = JabatanSerializers
+    permission_classes = (IsAuthenticated,)
