2093ab3ad765aa576831a765881dcabbcf582900
Spectre <bowo@anakdesa.id>
2019-07-04 18:09:10 +0700

    Add queryset

--- backend/goverment/views.py
+++ backend/goverment/views.py
@@ -1,21 +1,31 @@
-from rest_framework import viewsets
+from rest_framework import viewsets, authentication
 from rest_framework.permissions import (IsAuthenticated,
                                         )
 from .serializers import KelembagaanSerializers, JabatanSerializers
 from .models import Kelembagaan, Jabatan
+from .permissions import IsRegencyKelembagaan
 
 
 class KelembagaanView(viewsets.ModelViewSet):
     """Create kelembagaan in the system"""
-    queryset = Kelembagaan.objects.all()
     serializer_class = KelembagaanSerializers
-    permission_classes = (IsAuthenticated,)
+    permission_classes = [IsAuthenticated, IsRegencyKelembagaan]
+    authentication_classes = (authentication.TokenAuthentication,)
+
+    def get_queryset(self):
+        queryset = Kelembagaan.objects.all()
+        return queryset
+
+    def perform_create(self, serializer):
+        user_kelembagaan = self.request.user
+        serializer.save(user_kelembagaan=user_kelembagaan)
 
 
 class JabatanView(viewsets.ModelViewSet):
     """Create Jabatan in the system"""
     serializer_class = JabatanSerializers
-    permission_classes = (IsAuthenticated,)
+    permission_classes = (IsAuthenticated, )
+    authentication_classes = (authentication.TokenAuthentication,)
 
     def get_queryset(self):
         queryset = Jabatan.objects.all()
