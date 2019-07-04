38091053e6d51cac442a11d6e2592c96c16ad05a
Spectre <bowo@anakdesa.id>
2019-07-04 18:09:55 +0700

    Add get queryset

--- backend/userprofiles/views.py
+++ backend/userprofiles/views.py
@@ -66,9 +66,6 @@ class ProfileStatusViewSet(ModelViewSet):
 
     def get_queryset(self):
         queryset = ProfileStatus.objects.all()
-        username = self.request.query_params.get("username", None)
-        if username is not None:
-            queryset = queryset.filter(user_profile__user__username=username)
         return queryset
 
     def perform_create(self, serializer):
