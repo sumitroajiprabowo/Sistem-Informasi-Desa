1c9bfb06b0a15821fed1fb7d28384c96f17d5b35
Spectre <bowo@anakdesa.id>
2019-07-22 14:32:24 +0700

    Add Router

--- backend/goverment/urls.py
+++ backend/goverment/urls.py
@@ -1,12 +1,23 @@
 from django.urls import include, path
 from rest_framework.routers import DefaultRouter
-from .views import KelembagaanView, JabatanView
+from .views import (KelembagaanViewsetRegency, KelembagaanReadOnlyViewset,
+                    JabatanViewSetRegency, JabatanReadOnlyViewSet,
+                    CreateVillageGovermentView, VillageGovermentViewSet)
 
 router = DefaultRouter()
-router.register(r"kelembagaan", KelembagaanView, base_name='kelembagaan')
-router.register(r"jabatan", JabatanView, base_name='jabatan')
-
+router.register('kelembagaan/list', KelembagaanReadOnlyViewset,
+                base_name='kelembagaan-list')
+router.register("kelembagaan", KelembagaanViewsetRegency,
+                base_name='kelembagaan')
+router.register("jabatan/list", JabatanReadOnlyViewSet,
+                base_name='jabatan-list')
+router.register(r"jabatan", JabatanViewSetRegency,
+                base_name='jabatan')
+router.register("testbos", VillageGovermentViewSet,
+                base_name='pemdes')
 
 urlpatterns = [
     path("", include(router.urls)),
+    path('village/create/', CreateVillageGovermentView.as_view(),
+         name='create-goverment-village'),
 ]
