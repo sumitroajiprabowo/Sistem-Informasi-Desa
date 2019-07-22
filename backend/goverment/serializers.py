079527a393d1f7f3cc18a2ef571c34dd9a557c15
Spectre <bowo@anakdesa.id>
2019-07-22 14:32:44 +0700

    Fix Serializers Pemerintahan Desa

--- backend/goverment/serializers.py
+++ backend/goverment/serializers.py
@@ -1,18 +1,35 @@
 from rest_framework import serializers
-from .models import Kelembagaan, Jabatan
+from .models import Kelembagaan, Jabatan, Pemerintahan
 
 
 class KelembagaanSerializers(serializers.ModelSerializer):
-    user_kelembagaan = serializers.StringRelatedField(
-        read_only=True)
 
     class Meta:
         model = Kelembagaan
-        fields = ("id", "name", "user_kelembagaan")
+        fields = ('id', 'name',)
 
 
 class JabatanSerializers(serializers.ModelSerializer):
 
     class Meta:
         model = Jabatan
+        fields = "__all__"
+
+
+class PemerintahanSerializers(serializers.ModelSerializer):
+
+    # kelembagaan = serializers.PrimaryKeyRelatedField(
+    #     many=True,
+    #     queryset=Kelembagaan.objects.all()
+    # )
+    # jabatan = serializers.PrimaryKeyRelatedField(
+    #     many=True,
+    #     queryset=Jabatan.objects.all()
+    # )
+    # kelembagaan = KelembagaanSerializers(many=True)
+    # jabatan = JabatanSerializers(many=True)
+
+    class Meta:
+        model = Pemerintahan
+        read_only_fields = ('id', 'user')
         fields = "__all__"
